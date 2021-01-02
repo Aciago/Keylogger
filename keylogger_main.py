8b0d91e1db6875a98236c1361c6a4b7ce04a21cf
Aciago <aciagomk@gmail.com>
2021-01-02 12:07:34 +0100

    Initial commit

---
+++ keylogger_main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  2 00:07:02 2021
+
+@author: Aciago
+
+Keylogger app
+
+Usage:
+    gather statistics about keyboard and mouse usage of a user.
+    Tool is built for persnal use, general statistic purpose and python practice
+"""
+
+!pip install pynput
